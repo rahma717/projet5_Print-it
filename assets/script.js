b4f6bd33f5a8a3d3a79a580b6aeb9550dd44b72f
rahma717 <rahmabouabid2@gmail.com>
2023-03-06 11:00:18 +0100

    projet fini

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
+const arrowRightElement = document.querySelector(".arrow_right");
+const arrowLeftElement = document.querySelector(".arrow_left");
+const divDotsElement = document.querySelector(".dots");
+const bannerimg =document.querySelector(".banner-img");
+const bannertext =document.querySelector("#banner p");
+const alldot =document.getElementsByClassName("dot");
+var selectedImage=1;
+var selectedText="";
+var slectedscr="";
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -15,4 +24,58 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+
+arrowRightElement.addEventListener("click", myFunctionRight);
+
+function myFunctionRight() {
+
+	selectedImage=selectedImage+1;
+	if(selectedImage==slides.length+1){
+		selectedImage=1;
+	}
+	selectedText=slides[selectedImage-1].tagLine;
+	slectedscr=slides[selectedImage-1].image;
+	changeImageAndText();
+	addAndRemoveSelectedDot();
+
+
+}
+arrowLeftElement.addEventListener("click", myFunctionLeft);
+
+function myFunctionLeft() {
+	selectedImage=selectedImage-1;
+	if(selectedImage==0){
+		selectedImage=slides.length;
+	}
+	selectedText=slides[selectedImage-1].tagLine;
+	slectedscr=slides[selectedImage-1].image;
+	changeImageAndText();
+	addAndRemoveSelectedDot();
+
+}
+// changement des images avec les textes
+function changeImageAndText() {
+	bannerimg.src="./assets/images/slideshow/"+slectedscr;
+	bannertext.innerHTML=selectedText;
+}
+// ajout et suppression du SelectedDot
+function addAndRemoveSelectedDot() {
+	for (let i = 0; i < alldot.length; i++) {
+		alldot[i].classList.remove("dot_selected");
+	}
+	alldot[selectedImage-1].classList.add("dot_selected");
+}
+// ajout des dots
+for (let i = 0; i < slides.length; i++) {
+	var dottoadded= document.createElement("span");
+	dottoadded.classList.add("dot");
+	if(i==selectedImage-1){
+		dottoadded.classList.add("dot_selected");
+	}
+	divDotsElement.appendChild(dottoadded);
+  }
+
+
+
